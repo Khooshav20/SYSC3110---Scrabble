4688d647f872f6a0978a9619d10ce4901ea51af1
Khooshav <157403750+Khooshav20@users.noreply.github.com>
2024-10-22 19:44:04 +0000

    Work on board.java

--- Board.java
+++ Board.java
@@ -8,6 +8,18 @@
  * @author Khooshav Bundhoo (101132063)
  */
 public class Board {
+    class Move {
+        boolean isHorizontal;   // false = vertical
+        int row;    // 1 ... 15
+        int col;    // A=1, B=2 ... O=15
+
+        Move(boolean isHorizontal, int row, int col) {
+            this.isHorizontal = isHorizontal;
+            this.row = row;
+            this.col = col;
+        }
+    }
+
     private Square[][] board;
     private static final char[] COLUMN_LABELS = "ABCDEFGHIJKLMNO".toCharArray(); // Columns a-o, represented as A-O
 
@@ -33,28 +45,31 @@ public class Board {
      * @param score The score for the word
      * @return A formatted string for the move in "WORD xy +score" notation
      */
-    public String playMove(String word, int row, char col, String direction, int score) {
-        // Convert column letter to a 0-based index
-        int colIndex = Character.toUpperCase(col) - 'A';
-        int rowIndex = row - 1; // Convert to 0-based row index
-
-        if (!isValidMove(word, rowIndex, colIndex, direction)) {
-            return "Invalid move";
+    public int playMove(Tile[] tiles, String location) {
+        if (!isValidMove(tiles, location)) {
+            return 0;
         }
 
+        int score = 0;
+        Move move = getPlayNotation(location);
+
+        // Convert col and row to a 0-based index
+        int colIndex = move.col - 1;
+        int rowIndex = move.row - 1;
+        
         // Place the word on the board
-        if (direction.equals("H")) {
-            for (int i = 0; i < word.length(); i++) {
-                board[rowIndex][colIndex + i].setLetter(word.charAt(i));
-            }
-        } else if (direction.equals("V")) {
-            for (int i = 0; i < word.length(); i++) {
-                board[rowIndex + i][colIndex].setLetter(word.charAt(i));
+        for (int i = 0; i < tiles.length; i++) {
+            Tile tile = tiles[i];
+            score += tile.getScore();
+
+            if (move.isHorizontal) {
+                board[rowIndex][colIndex + i] = tile;
+            } else {
+                board[rowIndex + i][colIndex] = tile;
             }
         }
 
-        String coordinate = getPlayNotation(row, col, direction);
-        return String.format("%s %s +%d", word.toUpperCase(), coordinate, score);
+        return score;
     }
 
     /**
@@ -66,12 +81,42 @@ public class Board {
      * @param direction "H" for horizontal, "V" for vertical
      * @return true if the placement is valid, false otherwise
      */
-    private boolean isValidMove(String word, int row, int col, String direction) {
-        if (direction.equals("H")) {
-            return col + word.length() <= 15 && row >= 0 && row < 15;
-        } else if (direction.equals("V")) {
-            return row + word.length() <= 15 && col >= 0 && col < 15;
+    public boolean isValidMove(Tile[] tiles, String location) {
+        Move move = getPlayNotation(location);
+
+        if (move == null) {
+            return false;
         }
+        // Convert col and row to a 0-based index
+        int colIndex = move.col - 1;
+        int rowIndex = move.row - 1;
+
+        if (move.isHorizontal) {
+            // row within 1 and 15
+            // col + length of word - 1 still within board (O=15)
+            if (move.row >= 1 && move.row <= 15 && move.col + tiles.length - 1 <= 15) {
+                for (int i = 0; i < tiles.length; i++) {
+                    if (!(board[rowIndex][colIndex + i] instanceof BlankSquare)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            return false;
+        } 
+        
+        // col within 1 and 15 (A=1 ... O=15)
+        // row + length of word - 1 still within board
+        if (move.col >= 1 && move.col <= 15 && move.row + tiles.length - 1 <= 15) {
+            for (int i = 0; i < tiles.length; i++) {
+                if (!(board[rowIndex + i][colIndex] instanceof BlankSquare)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
         return false;
     }
 
@@ -87,30 +132,39 @@ public class Board {
         for (int i = 0; i < 15; i++) {
             sb.append(String.format("%2d", i + 1)).append(" ");
             for (int j = 0; j < 15; j++) {
-                sb.append(board[i][j].getLetter()).append(" ");
+                Square square = board[i][j];
+                sb.append(square instanceof BlankSquare ? " " : ((Tile) square).getLetter()).append(" ");
             }
             sb.append("\n");
         }
         return sb.toString();
     }
 
-    /** Plays are usually notated in the form "WORD xy +score" where WORD indicates the main word played,
-     *  xy is the coordinate of the first letter of the main word,
+    /** Plays are usually notated in the form "WORD xy" where WORD indicates the main word played,
+     *  xy is the coordinate of the first letter of the main word
      *
-     *
-     * @param row The starting row (1-based)
+     * @param location The starting row (1-based)
      * @param col The starting column (character A-O)
      * @param direction "H" for horizontal, "V" for vertical
      * @return The formatted coordinate as per the Scrabble rules
      */
-    private String getPlayNotation(int row, char col, String direction) {
-        if (direction.equals("H")) {
-            // Row comes first, then column letter (e.g., 15K)
-            return String.format("%d%c", row, Character.toUpperCase(col));
-        } else if (direction.equals("V")) {
-            // Column letter comes first, then row (e.g., K15)
-            return String.format("%c%d", Character.toUpperCase(col), row);
+    private Move getPlayNotation(String location) {
+        Move move = null;
+
+        // Horizontal (15K)
+        if (location.matches("([1-9]|1[0-5])[A-O]")) {
+            int row = Integer.parseInt(location.substring(0, location.length() - 1));
+            int col = location.charAt(location.length() - 1) - 'A' + 1;
+            move = new Move(true, row, col);
         }
-        return "";
+
+        // Vertical (K15)
+        if (location.matches("[A-O]([1-9]|1[0-5])")) {
+            int row = Integer.parseInt(location.substring(1));
+            int col = location.charAt(0) - 'A' + 1;
+            move = new Move(false, row, col);
+        }
+
+        return move;
     }
 }
